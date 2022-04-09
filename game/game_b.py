eb2d4649c1c6bb2a391f3b0748fd3dcb7aa8667c
Rikhil Fellner <rfellner@unc.edu>
2022-04-09 02:52:27 -0400

    progress on game

--- game/game_b.py
+++ game/game_b.py
@@ -17,8 +17,8 @@ running = True
 # rect2: pygame.Rect = pygame.Rect(600, 600, 50, 50)
 
 bullet: list[pygame.Rect] = list()
-blt = Bullet(600, 20, 10, 10)
-rect: pygame.Rect = blt.getRect()
+blt: list[Bullet] = list()  # Bullet(randint(0, 600), randint(0, 600), 10, 10)
+# rect: pygame.Rect = blt.getRect()
 while running:
     clock.tick(60)
 
@@ -29,22 +29,34 @@ while running:
 
    
     # pygame.display.flip()
-    
+    for item in blt:
+        rect: pygame.Rect = item.getRect()
+        pygame.draw.rect(screen, (255,255,255), rect)
+        
+        hit: bool = rect.colliderect(rect)
+        if hit:
+            blt.remove(item)
+        item.move()
+        # if hit:
+        #     bullet.remove(blt)
 
     for event in pygame.event.get():
+        fired: bool = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_e:
                 
+                blt.append(Bullet(randint(0, 600), randint(0, 600), 10, 10))
+                for i in range(0, len(blt)):
+                    bullet.append(pygame.Rect(blt[i].x, blt[i].y, 50, 50))
                 
-                for item in bullet:
-                    pygame.draw.rect(screen, (255,255,255), rect)
-                
-                bullet.append(pygame.Rect(600, 20, 50, 50))
+                fired = True
+        
                 
             
 
         if event.type == pygame.QUIT:
             running = False
+    # blt.move()
     pygame.display.flip()
         
 
